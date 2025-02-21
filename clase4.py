7c38c041e5a23d1ecd180138ab6fd93cd7260b96
Sebastianatehortua <juansebastianate@gmail.com>
2025-02-20 22:37:52 -0500

    avances

---
+++ clase4.py
@@ -0,0 +1,23 @@
+name = 'sebas'
+last_name='atehortua'
+caracter="c"
+print(type(caracter))
+
+print(name[0])
+print(name[1])
+print(name[-1])
+
+print (name + ' ' + last_name)
+
+print(len(name))
+
+print(name.lower())
+print(name.upper())
+print(last_name.strip())
+
+
+
+
+
+
+
